251adc8f061ff7de0fa65cb152143156dcffc45e
Miron Barykin <barykinmiron@gmail.com>
2023-11-01 17:12:14 +0100

    Implemented user approvement validation

--- bot/bot.py
+++ bot/bot.py
@@ -1,24 +1,53 @@
 import os
+
+import telegram
 from telegram import Update
-from telegram.ext import Application, CommandHandler, CallbackContext
-from bot.utils import Storage, InvalidStorageStructureException
+from telegram.ext import Application, CommandHandler, MessageHandler, CallbackContext
+from bot.utils import Storage
 
 storage = Storage('storage')
-try:
-    storage.validate()
-except InvalidStorageStructureException as error:
-    raise error
 
 TOKEN = os.environ.get("TELEGRAM_API_TOKEN")
 
 application = Application.builder().token(TOKEN).build()
 
 
-async def start(update: Update, context: CallbackContext):
+def unique_user(user: telegram.User):
+    if not storage.get_user(user.id):
+        storage.create_user(user.id, user.full_name)
+        return True
+    else:
+        return False
+
+
+def approved_user(user: telegram.User):
+    if storage.get_user(user.id).get('permission') != '0':
+        return True
+    else:
+        return False
+
+
+async def on_start(update: Update, context: CallbackContext):
     user = update.effective_user
+
+    if unique_user(user):
+        await update.message.reply_text("We are creating account for you. Wait for approval.")
+
     await update.message.reply_text(f"Hello, {user.first_name}! We're currently developing, stay tuned!")
 
 
-application.add_handler(CommandHandler("start", start))
+async def on_message(update: Update, context: CallbackContext):
+    user = update.effective_user
 
+    if unique_user(user):
+        await update.message.reply_text("We are creating account for you. Wait for approval.")
+    else:
+        if approved_user(user):
+            await update.message.reply_text(f"Nice to see you back, {user.first_name}!")
+        else:
+            await update.message.reply_text("Your account is not yet approved.")
+
+
+application.add_handler(CommandHandler("start", on_start))
+application.add_handler(MessageHandler(None, on_message))
 application.run_polling()
--- bot/utils/storage.py
+++ bot/utils/storage.py
@@ -1,4 +1,7 @@
 import os
+import csv
+
+USER_PROPERTIES = ['id', 'name', 'permission', 'date']
 
 
 class InvalidStorageStructureException(Exception):
@@ -9,20 +12,35 @@ class InvalidStorageStructureException(Exception):
 
 class Storage:
     def __init__(self, storage_path):
-        self.storage_path = storage_path
+        self.__storage_path = storage_path
+        self.__validate()
 
-    def validate(self, forced: bool = True):
+    def __validate(self, forced: bool = True):
+        self.__subjects_folder = os.path.join(self.__storage_path, 'subjects')
+        self.__students_file = os.path.join(self.__storage_path, 'students.csv')
 
-        subjects_folder = os.path.join(self.storage_path, 'subjects')
-        if not os.path.exists(subjects_folder) or not os.path.isdir(subjects_folder):
+        if not os.path.exists(self.__subjects_folder) or not os.path.isdir(self.__subjects_folder):
             if forced:
-                os.makedirs(subjects_folder)
+                os.makedirs(self.__subjects_folder)
             else:
                 raise InvalidStorageStructureException('Invalid storage structure. Subjects folder is missing.')
-        students_file = os.path.join(self.storage_path, 'students.csv')
-        if not os.path.exists(students_file) or not os.path.isfile(students_file):
+
+        if not os.path.exists(self.__students_file) or not os.path.isfile(self.__students_file):
             if forced:
-                with open(students_file, 'w', newline=''):
+                with open(self.__students_file, 'w', newline=''):
                     pass
             else:
                 raise InvalidStorageStructureException('Invalid storage structure. Students file missing.')
+
+    def create_user(self, user_id, user_name):
+        with open(self.__students_file, 'a', newline='') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=USER_PROPERTIES)
+            writer.writerow({'id': user_id, 'name': user_name, 'permission': 0, 'date': None})
+
+    def get_user(self, user_id):
+        with open(self.__students_file, 'r') as csvfile:
+            reader = csv.DictReader(csvfile, fieldnames=USER_PROPERTIES)
+            for row in reader:
+                if row['id'] == str(user_id):
+                    return row
+        return False
